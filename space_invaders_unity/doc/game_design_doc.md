b9a535f65c58408fd90a7ade4b98f4bebc248948
bruno <bruno.costa@cinnecta.com>
2020-06-21 19:28:38 -0300

    :rocket: Movimentação do grid

---
+++ space_invaders_unity/doc/game_design_doc.md
@@ -0,0 +1,25 @@
+Nave principal ações:
+- move em duas direções, direita e esquerda
+- atira, um tiro por vez
+
+Nave é protegida por estruturas, que vão sendo destruídas pelos tiros dos inimigos
+
+Inimigos se movem em duas direções, levando em consideração o grid de inimigos
+
+Cada inimigo tem uma frequência de disparos
+
+Progressão na fase
+- A medida que os inimigos são destruidos eles se movem mais rápido e atiram com mais frequência
+   - 1/2 dos inimigos restantes
+     - aumento de velocidade por []
+     - aumento na frequência dos tiros por []
+- De tempos em tempos aparece uma nava maior que vale mais pontos
+- De tempos em tempos um novo inimigo aparece no grid
+
+Pontuação
+- Cada inimigo destruido vale [] pontos
+- Cada inimigo especial vale [] pontos
+- A quantidade de defesas restantes
+- O tempo total da fase
+
+Tela de Pontuação (LeaderBoard)
---
+++ space_invaders_unity/space_invaders/Assets/Scripts/EnemyMoviment.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMoviment : MonoBehaviour {
+
+    [SerializeField] public GameObject[] aliens;
+
+    int[,] enemyGrid;
+
+    Vector3 direction;
+
+    void Start() {
+
+        var height = 3;
+        var width = 10;
+
+        direction = new Vector3(1, 0);
+
+        enemyGrid = new int[width, height];
+
+        var boxCollider = GetComponent<BoxCollider>();
+        boxCollider.size = new Vector3(width, height);
+
+        for(int i = 0; i < width; i++) {
+            for(int j = 0; j < height; j++) {
+                enemyGrid[i, j] = 1;
+                Instantiate(aliens[j], new Vector3(i + .5f - width / 2, j - height / 2) + transform.position, Quaternion.identity, transform);
+            }
+        }
+
+    }
+
+    float movimentInterval = 0f;
+    void Update() {
+        movimentInterval += Time.deltaTime;
+
+        if(movimentInterval >= 1) {
+            movimentInterval = 0;
+            transform.Translate(direction);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("coliiidiu");
+        direction *= -1;       
+    }
+}
